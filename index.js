c98053c81ae95ecdf2c9ff38f1dda79abb6ca8be
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-05-14 12:00:08 -0500

    setting up express, node, nodemon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("doggy");
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from node API, kjo");
+});
