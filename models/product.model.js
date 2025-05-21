b334e6be3d3f7450fd0b02ac505f3bed4f9073a8
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-05-21 14:39:28 -0500

    created mongoose schema and model

--- models/product.model.js
+++ models/product.model.js
@@ -4,7 +4,31 @@ const ProductSchema = mongoose.Schema(
     {
         name: {
             type: String,
-        }
-    }
+            required: [true, "Enter a name, its required"]
+        },
 
+        quantity: {
+            type: Number,
+            required: true
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+
+        image: {
+            type: String,
+            required: false, 
+        },
+    },
+
+    {
+        timestamps: true, // creates & records updatedAt and createdAt timestamps
+    }
 );
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product; 
