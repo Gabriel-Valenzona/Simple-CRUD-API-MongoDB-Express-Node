e2dd85c8ef7ef30e4bd1d41db82bdb58a2237148
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-05-22 11:57:35 -0500

    okokokok

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Product = require('/models/product.model.js');
 
 app.get('/', (req, res) => {
     res.send("Hello from node API, kjo");
+});
+
+app.use(express.json());
+
+app.post('/api/products', async (req, res) => {
+try{
+    const prouct = await Product.create(req.body);
+    res.status(200).json(product);
+}
+catch (error) {
+res.status(500).json({message: error.message});
+}
+
 });
 
 mongoose.connect("mongodb+srv://imgabing:imgabing@backenddb.hjiczan.mongodb.net/Node-API-Beginner?retryWrites=true&w=majority&appName=BackendDB")
